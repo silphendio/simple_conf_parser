14d8692adb272a5b9a7a2af51994504d8e33d6be
Silphendio <markus.ginthoer@gmx.at>
2024-03-09 06:21:45 +0100

    added c-style comments, tokenizer uses regex now

--- src/sscl/tokenizer.py
+++ src/sscl/tokenizer.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 
-# tokenize
+import re
+# single or double quotes, re.DOTALL for multiline strings
+re_string = re.compile(r"""("([^\\"]|(\\.))*")|('([^\\']|(\\.))*')""", re.DOTALL)
+
+# matches all except reserved characters or start of comments
+re_chunk = re.compile(r"([^\s\[\]{},;:=#'\"]|(/[^/*]))*")
 
 ARR_START, ARR_END, OBJ_START, OBJ_END, OBJ_ASSIGN, STRING, CHUNK = range(7)
 TOKEN_NAMES = ["ARR_START", "ARR_END", "OBJ_START", "OBJ_END", "OBJ_ASSIGN", "STRING", "CHUNK"]
@@ -26,58 +31,50 @@ def tokenize(conf_str):
         c = conf_str[i]
         if c == '[':
             tokens.append(make_token(i, ARR_START))
+
         elif c == ']':
             tokens.append(make_token(i, ARR_END))
+        
         elif c == '{':
             tokens.append(make_token(i, OBJ_START))
+
         elif c == '}':
             tokens.append(make_token(i, OBJ_END))
+
         elif c in ":=":
             tokens.append(make_token(i, OBJ_ASSIGN))
+
         elif c in "'\"":
-            j = _find_unescaped_match(conf_str, i+1, c)
-            tokens.append(make_token(i, STRING, conf_str[i+1: j]))
-            i = j
-        elif c == '#': # comment
-            i = _find_line_end(conf_str, i+1) 
+            m = re_string.match(conf_str, i)
+            if m:
+                tokens.append(make_token(i, STRING, m.group()[1:-1]))
+                i = m.end() - 1
+            else:
+                err(i, "no matching quote found ")
+
+        elif c == '#' or conf_str[i:i+2] == '//': # comment
+            i = conf_str.find('\n', i)
+            if i < 0: 
+                i = len(conf_str) - 1
+        elif conf_str[i:i+2] == '/*': # multi-line comment
+            i = conf_str.find('*/', i) + 1
+            if i < 0: 
+                i = len(conf_str) - 1
+
         elif c.isspace() or c in ",;":
             # ignore whitespace and commas, semicolons too for good measure
             pass
         else :
-            j = _find_chunk_end(conf_str, i+1) 
-            tokens.append(make_token(i, CHUNK, conf_str[i: j]))
-            i = j-1
+            m = re_chunk.match(conf_str, i)
+            if m:
+                tokens.append(make_token(i, CHUNK, m.group()))
+                i = m.end() - 1
+            else:
+                tokens.append(make_token(i, CHUNK, conf_str[i:]))
+                i = len(conf_str) - 1
             
         i += 1
     return tokens
-
-# this needs to handle strings like "\\\\\\\"" too
-def _find_unescaped_match(s: str, i: int, c) -> int:
-    j = i # for error handling
-    escape = False
-    while i < len(s):
-        if s[i] == '\\':
-            escape = not escape
-        elif s[i] == c and not escape:
-            return i
-        else:
-            escape = False
-        i += 1
-    err(j, "no matching quote found " + s[j:])
-
-def _find_line_end(s: str, i: int) -> int:
-    while i < len(s):
-        if s[i] == '\n':
-            return i
-        i += 1
-    return i
-
-def _find_chunk_end(s: str, i: int) -> int:
-    while i < len(s):
-        if s[i].isspace() or s[i] in "[]{},;:=#'\"":
-            return i
-        i += 1
-    return i
 
 def err(index: int, msg: str):
     raise ValueError({"index":index , "msg": msg})
